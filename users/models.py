55071b50bee6dad92b1ea29b02a086c3779336ed
Shohrukh Kholboev <kholboevshohrukh@gmail.com>
2024-11-06 15:19:30 +0500

    Create config users app

--- users/models.py
+++ users/models.py
@@ -1,10 +1,12 @@
 from datetime import datetime, timedelta
 
+import uuid
 from PIL.ImagePalette import random
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import FileExtensionValidator
 from django.db import models
-
+from django.utils.regex_helper import normalize
+from rest_framework_simplejwt.tokens import RefreshToken
 
 ORDINARY_USER, MANAGER, ADMIN = ("ordinary_user", "manager", "admin")
 VIA_EMAIL, VIA_PHONE = ("via_email", "via_phone")
@@ -50,6 +52,45 @@ class User(AbstractUser):
             verify_type=verify_type,
             code=code,
         )
+
+    def check_username(self):
+        if not self.username:
+            temp_username = f"instagram-{uuid.uuid4().__str__().split('-')[-1]}"
+            while User.objects.filter(username=temp_username):
+                temp_username = f"{temp_username}{random.randint(0,9)}"
+            self.username = temp_username
+
+    def check_email(self):
+        if self.email:
+            normalize_email = self.email.lower()
+            self.email = normalize_email
+
+    def check_pass(self):
+        if not self.password:
+            temp_password = f"password-{uuid.uuid4().__str__().split('-')[-1]}"
+            self.password = temp_password
+
+    def hashing_password(self):
+        if not self.password.startswith('pbkdf2_sha256'):
+            self.set_password(self.password)
+
+    def token(self):
+        refresh = RefreshToken.for_user(self)
+        return {
+            "access": str(refresh.access_token),
+            "refresh_token": str(refresh)
+        }
+
+    def clean(self):
+        self.check_email()
+        self.check_username()
+        self.check_pass()
+        self.hashing_password()
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.clean()
+        super(User, self).save(*args, **kwargs)
 
 
 PHONE_EXPIRE = 2
